243739bbd583c3c822630d7ea0bc84b8d9aca831
gavrilasa <gavrilasamanaa@gmail.com>
2025-10-24 15:28:27 +0700

    chore(project): Boilerplate Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Al Madeena Islamic School",
+  description: "Al Madeena Islamic School",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Al Madeena" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
