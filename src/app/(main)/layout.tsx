01e4e5e1e21400a2f633f13123c3ac7732571ee8
gavrilasa <gavrilasamanaa@gmail.com>
2025-10-24 21:29:20 +0700

    fix(project): Fix Up Boilerplate

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Al Madeena Islamic School",
@@ -9,16 +9,15 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
+const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${inter.className}`}>
       <head>
         <meta name="apple-mobile-web-app-title" content="Al Madeena" />
       </head>
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16"></div>
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8"></div>
     </main>
   );
 }
